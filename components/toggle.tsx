0af298cb1558940b37aec249d514d94e5fabb593
saki <16546851+sakiko-2@users.noreply.github.com>
2022-05-27 14:28:40 +1200

    add toggle switch

---
+++ components/toggle.tsx
@@ -0,0 +1,26 @@
+interface Props {
+  checked: boolean,
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void,
+}
+
+const Toggle = ({ checked, handleChange }: Props) => (
+  <label className="flex justify-between items-center p-2 text-sm relative">
+    <span aria-hidden="true">Annually</span>
+    <input
+      type="checkbox"
+      className="appearance-none peer absolute left-0 top-0 w-full h-full rounded-md"
+      onChange={handleChange}
+      checked={checked}
+    />
+    <span
+      className="w-16 h-10 flex items-center mx-4 p-1 bg-[color:var(--primary-color)]
+      rounded-full after:w-8 after:h-8 after:bg-white
+      after:rounded-full after:shadow-md after:duration-300 ease-in-out
+      peer-checked:after:translate-x-6"
+    >
+    </span>
+    Monthly
+  </label>
+)
+
+export default Toggle
