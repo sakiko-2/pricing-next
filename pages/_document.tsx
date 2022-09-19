772e15410b61cd3e9a4a16924fb6515476223882
saki <16546851+sakiko-2@users.noreply.github.com>
2022-09-19 23:26:40 +1200

    Remove extra links

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,8 +9,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap"
           rel="stylesheet"
